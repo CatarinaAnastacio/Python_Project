51beb06579e2b5b186acd53669a5a961a445f24d
Lanjing <s256855@dtu.dk>
2026-04-06 16:57:11 +0200

    This is the first code program to get the k_mer from both foward and reverse strand

---
+++ 19_mer.py
@@ -0,0 +1,88 @@
+
+def reading_dnaFasta (filename):
+    Headers = []
+    sequences = []
+
+    infile = open(filename, 'r')
+    seq = ''
+    for line in infile:
+        line = line.strip()
+        if line and line[0] == '>':
+            head = line[1:]
+            Headers.append(head)
+            if seq != '':
+                sequences.append(seq)
+                seq = ''
+        else:
+            seq += line 
+    if seq != '':
+            sequences.append(seq)
+
+    infile.close()
+
+    return Headers, sequences
+
+Headers, sequences = reading_dnaFasta('resistance_genes.fsa.txt')
+
+def reverse_com_strand(sequence):
+
+    complementary_strand = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    
+    reverse_com_strand = '' 
+
+    for base in sequence[::-1]:
+        if base in complementary_strand:
+            reverse_com_strand += complementary_strand[base]
+        else:
+            reverse_com_strand += 'N'
+
+    return reverse_com_strand
+
+reverse_complementary = []
+
+for seq in sequences:
+    rev_com = reverse_com_strand(seq)
+    reverse_complementary.append(rev_com)
+
+def nineteen(DNA):
+    
+    nineteen_mers = set()
+
+    for i in range(len(DNA) - 18):
+        fragment = DNA[i:i+19]
+        nineteen_mers.add(fragment)
+    return nineteen_mers
+
+k_mer_fowardstrand = []
+k_mer_rev_com_strand = []
+
+for seq in sequences:
+    k = nineteen(seq)
+    k_mer_fowardstrand.append(k)
+
+for sequence in reverse_complementary:
+    rev = nineteen(sequence)
+    k_mer_rev_com_strand.append(rev)
+
+
+def k_mer_write(Headers, sequences, filename):
+    outfile = open(filename, 'w')
+
+    for i in range(len(sequences)):
+        header = Headers[i]
+        sequence = sequences[i]
+
+        k_mer_foward = nineteen(sequence)
+
+        rev_com = reverse_com_strand(sequence)
+        rev_com_strand = nineteen(rev_com)
+
+        outfile.write('>'+ header + '\n')
+        outfile.write('Foward 19-mer:'+'\n')
+        outfile.write(' '.join(k_mer_foward)+ '\n')
+        outfile.write('Revere 19_mer:'+'\n')
+        outfile.write(' '.join(rev_com_strand) + '\n\n')
+    
+    outfile.close
+
+k_mer_output = k_mer_write(Headers, sequences, 'output_k_mer')
